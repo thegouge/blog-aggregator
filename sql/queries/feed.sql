439fad265a4ad9e2d76a4efc01d9d56c132e9153
thegouge <thegouge42@gmail.com>
2024-05-11 20:48:34 -0700

    Writing a MarkFeedFetched query to the DB

--- sql/queries/feed.sql
+++ sql/queries/feed.sql
@@ -9,3 +9,9 @@ SELECT * FROM feeds;
 -- name: GetNextFeedsToFetch :many
 SELECT * FROM feeds ORDER BY last_fetched_at DESC LIMIT $1;
 
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET last_fetched_at = $2, updated_at = $2
+WHERE id = $1
+RETURNING *;
+
