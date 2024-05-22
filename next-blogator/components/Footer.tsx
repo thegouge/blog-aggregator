5b951414ac32451fd57bc828f27b0d88377297dc
thegouge <thegouge42@gmail.com>
2024-05-21 20:22:17 -0700

    Initializing the homepage

---
+++ next-blogator/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+			This is the Footer!
+		</footer>
+	)
+}
